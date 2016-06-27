a56f8776cb62a00d0529961de9b09211af09a9e9
Halil Eren ERTAN <halileren1996@hotmail.com>
2016-06-28 00:55:27 +0300

    Created Student class and added properties.

---
+++ Student.java
@@ -0,0 +1,17 @@
+/**
+ * Project: RoomManager
+ * <p>
+ * Date: 27 Jun 2016
+ * Author: eren
+ */
+public class Student {
+    private String name;
+    private String surname;
+    private String gender;
+    private int id;
+    private int roomNumber;
+    private String school;
+    private int StayingInRoom;
+    private int roomType;
+
+}
